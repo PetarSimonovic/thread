901364fe6fdbaabc99c6b93cd94a8ab1b73581fe
PetarSimonovic <petar@simonovic.com>
2021-04-23 16:42:17 +0100

    Initial set up: GameController and GameScene

---
+++ thread/GameElements.swift
@@ -0,0 +1,20 @@
+//
+//  GameElements.swift
+//  thread
+//
+//  Created by Petar Simonovic on 23/04/2021.
+//
+
+import SpriteKit
+
+struct collisionBitMask {
+    static let needleCategory:UInt32 = 0x1 << 0
+    static let wallCategory:UInt32 = 0x1 << 1
+    static let loopCategory:UInt32 = 0x1 << 2
+    static let groundCategory:UInt32 = 0x1 << 3
+}
+
+extension GameScene {
+    
+}
+
