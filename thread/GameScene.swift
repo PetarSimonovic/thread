85ce408fa6a557839197c1cb3f44050890c2dc17
PetarSimonovic <petar@simonovic.com>
2021-04-23 17:16:33 +0100

    Game variables set up

--- thread/GameScene.swift
+++ thread/GameScene.swift
@@ -8,7 +8,23 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var isGameStarted: Bool = false
+    var isDead: Bool = false
+    
+   // SOUND: let coinSound = SKAction.playSoundFileNamed("CoinSound.mp3", waitFor Completion: false)
+    
+    var score: Int = 0
+    var scoreLbl = SKLabelNode()
+    var highscoreLbl = SKLabelNode()
+    var taptoplayLbl = SKLabelNode()
+    var restartBtn = SKSpriteNode()
+    var pauseBtn = SKSpriteNode()
+    var logoImg = SKSpriteNode()
+    var wallPair = SKNode()
+    var moveAndRemove = SKAction()
+    
         
     
   //  let fabric = SKSpriteNode(color: .gray, size: CGSize(width: 200, height: 200))
