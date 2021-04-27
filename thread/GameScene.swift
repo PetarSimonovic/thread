b59764e1cb57b92ac380eb2498b3ba0a6e9f9304
PetarSimonovic <petar@simonovic.com>
2021-04-27 12:25:44 +0100

    dynamicaly generates background

--- thread/GameScene.swift
+++ thread/GameScene.swift
@@ -24,6 +24,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var logoImg = SKSpriteNode()
     var wallPair = SKNode()
     var moveAndRemove = SKAction()
+    var backgroundNode = 0
     
         
     
@@ -40,13 +41,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func update (_ currentTime: TimeInterval) {
         
         if isGameStarted == true {
-            if isDead == false {
+        createBackground()
+         if isDead == false {
                 enumerateChildNodes(withName: "background", using: ({
                     (node, error) in
                     let bg = node as! SKSpriteNode
                     bg.position = CGPoint(x: bg.position.x - 2, y: bg.position.y)
                     if bg.position.x <= -bg.size.width {
-                        bg.position = CGPoint(x:bg.position.x + bg.size.width * 2, y: bg.position.y)
+                        print(bg.position.x)
+                        bg.removeFromParent()
+                        self.backgroundNode -= 1
+                      //  bg.position = CGPoint(x:bg.position.x + bg.size.width * 2, y: bg.position.y)
                     }
                 }))
             }
@@ -54,28 +59,39 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 //
     }
     
-    func createScene() {
-        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
-        self.physicsBody?.categoryBitMask = CollisionBitMask.groundCategory
-        self.physicsBody?.collisionBitMask = CollisionBitMask.needleCategory
-        self.physicsBody?.contactTestBitMask = CollisionBitMask.needleCategory
-        self.physicsBody?.isDynamic = false
-        self.physicsBody?.affectedByGravity = false
-        
-        self.physicsWorld.contactDelegate = self
-        self.backgroundColor = SKColor (red: 80.0/255.0, green: 192.0/255.0, blue: 203.0/255.0, alpha: 1.0)
-        
-        for i in 0..<2 {
-            let background = SKSpriteNode(imageNamed: "background")
-            background.anchorPoint = CGPoint.init(x: 0, y: 0)
-            background.position = CGPoint(x: CGFloat(i) * self.frame.width, y: 0)
-            background.name = "background"
-            background.size = (self.view?.bounds.size)!
-            self.addChild(background)
-      
+    func createBackground() {
+      let  element = Int.random(in: 1..<100)
+        if element <= 9 && backgroundNode <= 8 {
+            print(element)
+        let background = SKSpriteNode(imageNamed: "background_\(element)")
+                background.anchorPoint = CGPoint.init(x: 0, y: 0)
+            let height = CGFloat(arc4random() % UInt32(CGFloat(self.frame.height)))
+                background.position = CGPoint(x: CGFloat(1) * self.frame.width, y: height)
+                background.name = "background"
+              let randomScale = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+              background.setScale(randomScale)
+            //background.zRotation = CGFloat(M_PI_4)
+              self.addChild(background)
+                backgroundNode += 1
+          
+            }
+            
+            
+            
         }
-        
-        
+    
+    
+    func createScene() {
+//        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+//        self.physicsBody?.categoryBitMask = CollisionBitMask.groundCategory
+//        self.physicsBody?.collisionBitMask = CollisionBitMask.needleCategory
+//        self.physicsBody?.contactTestBitMask = CollisionBitMask.needleCategory
+//        self.physicsBody?.isDynamic = false
+//        self.physicsBody?.affectedByGravity = false
+//
+//        self.physicsWorld.contactDelegate = self
+        self.backgroundColor = SKColor (red: 80.0/255.0, green: 192.0/255.0, blue: 203.0/255.0, alpha: 1.0)
+
 //        background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
 //
 //        addChild(background)
@@ -85,4 +101,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
 //
    }
+
 }
+
+
